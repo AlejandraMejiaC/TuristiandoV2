dc0f50e28ac58c7c61622c8b026794ef45c60eb2
diseño y desarrollo <disenoydesarrollo@academicos.cesde.edu.co>
2023-06-28 19:45:24 -0600

    se crea hoteles y strings

---
+++ app/src/main/java/com/example/turistiandov2/Sitios_Home.java
@@ -0,0 +1,14 @@
+package com.example.turistiandov2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Sitios_Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sitios_home);
+    }
+}
