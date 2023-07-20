dfa8200ac8aac642085d133c4ecfdec1b595274a
CamilaRod17 <maly1997.j@gmail.com>
2023-07-19 20:07:50 -0500

    se crea el acerca

--- app/src/main/java/com/example/turistiandov2/Home.java
+++ app/src/main/java/com/example/turistiandov2/Home.java
@@ -3,6 +3,7 @@ package com.example.turistiandov2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -10,6 +11,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.Locale;
 
 public class  Home extends AppCompatActivity {
 
@@ -19,6 +22,20 @@ public class  Home extends AppCompatActivity {
     Button sitios;
 
     TextView bienvenido;
+
+    public void cambiaridioma(String idioma){
+        //configurar el lenguaje del telefono
+        Locale lenguaje=new Locale(idioma);
+        Locale.setDefault(lenguaje);
+
+        //configuramos globalmente el telefono
+        Configuration configTelefono= getResources().getConfiguration();
+
+        configTelefono.locale=lenguaje;
+
+        //ejecutamos la config
+        getBaseContext().getResources().updateConfiguration(configTelefono,getBaseContext().getResources().getDisplayMetrics());
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,7 +48,7 @@ public class  Home extends AppCompatActivity {
         hoteles=findViewById(R.id.botonhoteles);
         restaurantes=findViewById(R.id.botonrestaurantes);
         sitios=findViewById(R.id.botonsitios);
-        bienvenido=findViewById(R.id.bienvenido);
+        bienvenido=findViewById(R.id.cliente);
 
         //uniendotexto del putextra CON EL TEXTVIEW
         bienvenido.setText(nombre);
@@ -76,16 +93,23 @@ public class  Home extends AppCompatActivity {
        switch (itemSeleccionado){
 
            case(R.id.opcion1):
-
-               break;
+               this.cambiaridioma("en");
+               Intent intentingles=new Intent(Home.this, Home.class);
+               startActivity(intentingles);
+           break;
 
            case(R.id.opcion2):
-
-               break;
+               this.cambiaridioma("es");
+               Intent intentes=new Intent(Home.this, Home.class);
+               startActivity(intentes);
+           break;
 
            case(R.id.opcion3):
-
+               this.cambiaridioma("it");
+               Intent intentItaliano=new Intent(Home.this, Home.class);
+               startActivity(intentItaliano);
                break;
+
 
            case(R.id.opcion4):
                     Intent intent=new Intent(Home.this,AcercaSolucion.class);
@@ -96,7 +120,5 @@ public class  Home extends AppCompatActivity {
        return super.onOptionsItemSelected(items);
     }
 
-    public void cambiaridioma(String idioma){
 
-    }
 }
