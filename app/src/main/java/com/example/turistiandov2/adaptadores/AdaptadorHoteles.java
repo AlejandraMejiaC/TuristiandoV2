7920822f0bbb4e23152fe4d06d0e650dd37c1597
manunovoa <manunovoa0415@gmail.com>
2023-08-29 20:51:33 -0500

    se crea ampliando

--- app/src/main/java/com/example/turistiandov2/adaptadores/AdaptadorHoteles.java
+++ app/src/main/java/com/example/turistiandov2/adaptadores/AdaptadorHoteles.java
@@ -1,5 +1,6 @@
 package com.example.turistiandov2.adaptadores;
 
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,6 +10,7 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.turistiandov2.AmpliandoHotel;
 import com.example.turistiandov2.Moldes.MoldeHotel;
 import com.example.turistiandov2.R;
 
@@ -67,6 +69,14 @@ public class AdaptadorHoteles extends RecyclerView.Adapter<AdaptadorHoteles.view
             nombreHotel.setText(moldeHotel.getNombre());
             precioHotel.setText(moldeHotel.getPrecio());
             contactoHotel.setText(moldeHotel.getTelefono());
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(itemView.getContext(), AmpliandoHotel.class);
+                    itemView.getContext().startActivity(intent);
+                }
+            });
 
         }
     }
